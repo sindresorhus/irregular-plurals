8672b3db46995e761784ea51e07954cdde49138f
Corey Farrell <git@cfware.com>
2019-04-30 08:01:01 -0400

    Require Node.js 8, fix map isolation (#16)

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
 import test from 'ava';
-import m from '.';
+import irregularPlurals from '.';
 
 test('main', t => {
-	t.true(m.has('calf'));
+	t.true(irregularPlurals.has('calf'));
+	t.is(irregularPlurals.get('calf'), 'calves');
+});
+
+test('isolated', t => {
+	const fresh1 = require('.');
+	const fresh2 = require('.');
+
+	fresh1.delete('calf');
+	t.false(fresh1.has('calf'));
+	t.true(fresh2.has('calf'));
+	t.true(irregularPlurals.has('calf'));
 });
