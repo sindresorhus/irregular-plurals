fb00015a2082108bf97a8837b575736f52211479
Corey Farrell <git@cfware.com>
2019-04-30 06:41:50 -0400

    Require Node.js 8, fix map isolation

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 'use strict';
 const irregularPlurals = require('./irregular-plurals.json');
 
-const map = new Map();
-// TODO: Use Object.entries when targeting Node.js 8
-for (const key of Object.keys(irregularPlurals)) {
-	map.set(key, irregularPlurals[key]);
-}
-
 // Ensure nobody can modify each others Map
 Object.defineProperty(module, 'exports', {
 	get() {
-		return map;
+		return new Map(Object.entries(irregularPlurals));
 	}
 });
